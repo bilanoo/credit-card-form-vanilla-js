4f518e4f52d462f7b1d8d018b5e3955fd2a81d74
bilanoo <blalkhan94@gmail.com>
2024-03-29 16:33:42 +0000

    implemented static card details

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ document.querySelector("#app").innerHTML = `
     <div class="view-card-parent">
       <div class='card-details-container'>
         <div class='card-back card-general-size'>
-
+          <p class="cvv2">000</p>
         </div>
         <div class='card-front card-general-size'>
-        
+          <div class="circle-container">
+            <div class="big-circle"></div>
+            <div class="small-circle"></div>
+          </div>
+
+          <p class="card-number">0000 0000 0000 0000</p>
+
+          <div class="card-name-expiry-container">
+          <p class="cardholder-name">JANE APPLESEED</p>
+          
+          <p class="expiry-date">00/00</p>
+          </div>
         </div>
       </div>
     </div>
