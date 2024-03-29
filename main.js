a73ad361e11ca29f942cf0c382e46a967a89d52d
bilanoo <blalkhan94@gmail.com>
2024-03-29 15:06:30 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import "./style.css";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div class="homepage-container">
+    <div class="view-card-parent">
+      <div class='card-details-container'>
+        <div class='card-back card-general-size'>
+
+        </div>
+        <div class='card-front card-general-size'>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+`;
